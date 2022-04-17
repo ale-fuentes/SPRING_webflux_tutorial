456d7b71ed962dd024a366b0eb26bdc0969aa8a4
ale fuentes <fuentesra@hotmail.com>
2022-04-17 20:30:27 -0300

    feat - add first files of project

---
+++ src/main/java/com/alefuuuu/webfluxtutorial/WebfluxtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.alefuuuu.webfluxtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxtutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alefuuuu/webfluxtutorial/WebfluxtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alefuuuu.webfluxtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

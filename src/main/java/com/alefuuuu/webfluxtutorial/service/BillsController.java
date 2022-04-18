9569e3dab9692017d100b444311d523164e849a6
ale fuentes <fuentesra@hotmail.com>
2022-04-18 04:17:23 -0300

    feat - add webflux no blocking request

--- src/main/java/com/alefuuuu/webfluxtutorial/service/BillsController.java
+++ src/main/java/com/alefuuuu/webfluxtutorial/service/BillsController.java
@@ -1,27 +1,24 @@
 package com.alefuuuu.webfluxtutorial.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alefuuuu.webfluxtutorial.domain.Bill;
 
+import reactor.core.publisher.Flux;
+
 @RestController
 public class BillsController {
 	
 	@GetMapping("/bill")
-	public List<Bill> bills(){
-		List<Bill> list = new ArrayList<Bill>();
-		list.add(new Bill(1, "computer", 200.50));
-		list.add(new Bill(2, "tablet", 150.55));
-		
-		try {
-			Thread.sleep(3000);
-		}catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	public Flux<Bill> bills(){
+		Flux<Bill> list = Flux
+				.just(
+						new Bill(1, "computer", 200.50),
+						new Bill(2, "tablet", 150.55))
+				.delaySequence(Duration.ofSeconds(3));
 		
 		return list;
 	}
